d2ed18b866ec8b06a6ceea13995f7d0c44027565
Wai Lee <64799897+gadgeteerX@users.noreply.github.com>
2020-09-30 12:42:29 +0100

    Amend button formatting

--- TEST27_WPF_Lotto/MainWindow_aux.cs
+++ TEST27_WPF_Lotto/MainWindow_aux.cs
@@ -25,13 +25,23 @@ namespace TEST27_WPF_Lotto
                 seq[i] = seq[j];                // swap array[random index] ...
                 seq[j] = k;                     // with [next index]
             }
-            box1.Text = seq[1].ToString();
-            box2.Text = seq[2].ToString();
-            box3.Text = seq[3].ToString();
-            box4.Text = seq[4].ToString();
-            box5.Text = seq[5].ToString();
-            box6.Text = seq[6].ToString();
+            box1.Text = seq[1].ToString().PadLeft(2, ' ');
+            box1.Text = box1.Text.Substring(box1.Text.Length - 2);
 
+            box2.Text = seq[2].ToString().PadLeft(2, ' ');
+            box2.Text = box2.Text.Substring(box2.Text.Length - 2);
+
+            box3.Text = seq[3].ToString().PadLeft(2, ' ');
+            box3.Text = box3.Text.Substring(box3.Text.Length - 2);
+
+            box4.Text = seq[4].ToString().PadLeft(2, ' ');
+            box4.Text = box4.Text.Substring(box4.Text.Length - 2);
+
+            box5.Text = seq[5].ToString().PadLeft(2, ' ');
+            box5.Text = box5.Text.Substring(box5.Text.Length - 2);
+
+            box6.Text = seq[6].ToString().PadLeft(2, ' ');
+            box6.Text = box6.Text.Substring(box6.Text.Length - 2);
 
             //MessageBox.Show("Generate numbers...", "Generate",
             //MessageBoxButton.OK, MessageBoxImage.Exclamation);
@@ -39,12 +49,12 @@ namespace TEST27_WPF_Lotto
 
         private void Reset()
         {
-            box1.Text = "--";
-            box2.Text = "--";
-            box3.Text = "--";
-            box4.Text = "--";
-            box5.Text = "--";
-            box6.Text = "--";
+            box1.Text = "__";
+            box2.Text = "__";
+            box3.Text = "__";
+            box4.Text = "__";
+            box5.Text = "__";
+            box6.Text = "__";
         }
     }
 }
